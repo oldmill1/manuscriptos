dbdb2a1294c000ac9297320988e6f5bc958969ec
Ankur Taxali <ataxali@gmail.com>
2025-11-23 00:33:38 -0500

    refactor(MenuBar): remove new document button Removed the 'New Document' button functionality from the MenuBar component to simplify the UI.

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -1,28 +1,6 @@
 <script lang="ts">
 	import styles from './MenuBar.module.scss';
-	import { createNewDocument } from '$lib/utils/documentUtils';
-
-	const menuItems = ['New Document'];
-
-	async function handleNewClick() {
-		await createNewDocument();
-	}
 </script>
 
 <div class={styles.menubar}>
-	{#each menuItems as item}
-		<button 
-			type="button"
-			class={styles.menuItem}
-			onclick={handleNewClick}
-			onkeydown={async (e) => {
-				if (e.key === 'Enter' || e.key === ' ') {
-					e.preventDefault();
-					await handleNewClick();
-				}
-			}}
-		>
-			{item}
-		</button>
-	{/each}
 </div>
