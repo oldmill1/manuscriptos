1e443ae504ed629a7067dca18a936855e95ec974
Ankur Taxali <ataxali@gmail.com>
2025-11-29 01:34:39 -0500

    refactor(breadcrumbs): update styling and structure Revised breadcrumb trail style with arrow shapes and improved current item highlight. Removed redundant separator logic from BreadcrumbTrail component.

--- src/lib/components/Explorer/BreadcrumbTrail/BreadcrumbTrail.svelte
+++ src/lib/components/Explorer/BreadcrumbTrail/BreadcrumbTrail.svelte
@@ -72,38 +72,18 @@
 {#if breadcrumbs.length > 0}
 	<div class={styles.breadcrumbContainer}>
 		{#each breadcrumbs as item, index}
-			{#if index > 0}
-				<span class={styles.separator}>
-					<svg fill="currentColor" viewBox="0 0 24 24" class={styles.icon}>
-						<path
-							clip-rule="evenodd"
-							d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-							fill-rule="evenodd"
-						></path>
-					</svg>
-				</span>
-			{/if}
-			
 			{#if index === breadcrumbs.length - 1}
-				<!-- Current folder - not clickable -->
+				<!-- Current folder - not clickable, highlighted -->
 				<span class={styles.current}>{item.name}</span>
 			{:else}
-				<!-- Parent folder - clickable -->
-				<Motion 
-					let:motion
-					whileHover={{ scale: 1.05 }}
-					whileTap={{ scale: 0.95 }}
-					transition={{ duration: 0.2, ease: [0.4, 0, 0.2, 1] }}
+				<!-- Parent folder - clickable arrow shape -->
+				<button 
+					class={styles.breadcrumbArrow}
+					onclick={() => handleBreadcrumbClick(item)}
+					type="button"
 				>
-					<button 
-						class={styles.breadcrumb}
-						onclick={() => handleBreadcrumbClick(item)}
-						type="button"
-						use:motion
-					>
-						{item.name}
-					</button>
-				</Motion>
+					{item.name}
+				</button>
 			{/if}
 		{/each}
 	</div>
